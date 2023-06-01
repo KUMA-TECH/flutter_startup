84a3aadddb2f4de304a9b727fc2332703279709f
huang pengfei <huangpengfei@qudian.com>
2023-06-01 16:05:44 +0800

    CHG menu dynamic configure

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_startup/app/screen/dashboard.dart';
 import 'package:flutter_startup/app/screen/main/main_screen.dart';
 import 'package:flutter_startup/app/state/MenuAppController.dart';
 import 'package:flutter_startup/config/global_config.dart';
 import 'package:flutter_startup/res/theme/theme.dart';
 import 'package:flutter_startup/utils/utilities.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -13,23 +11,18 @@ void main() {
       width: GlobalConfig.defaultWindowSize.width,
       height: GlobalConfig.defaultWindowSize.height);
 
-  runApp(MyApp());
+  runApp(const Launcher());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Launcher extends StatelessWidget {
+  const Launcher({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'bejson',
-      // theme: ThemeData.dark().copyWith(
-      //   scaffoldBackgroundColor: Theme.of(context).colorScheme.background,
-      //   textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
-      //       .apply(bodyColor: Colors.white),
-      //   canvasColor: Theme.of(context).colorScheme.secondary,
-      // ),
-      theme: ThemeProvider.dartTheme(),
+      theme: ThemeProvider.lightTheme(),
       darkTheme: ThemeProvider.dartTheme(),
       home: MultiProvider(
         providers: [
@@ -42,40 +35,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// class Launcher extends StatelessWidget {
-//   const Launcher({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeProvider.lightTheme(),
-//       darkTheme: ThemeProvider.dartTheme(),
-//       home: Scaffold(
-//         appBar: AppBar(
-//           leading: const IconButton(
-//             icon: Icon(Icons.menu),
-//             tooltip: 'Navigation menu',
-//             onPressed: null,
-//           ),
-//           title: const Text('Start up'),
-//           actions: const [
-//             IconButton(
-//               icon: Icon(Icons.search),
-//               tooltip: 'Search',
-//               onPressed: null,
-//             ),
-//           ],
-//         ),
-//         body: DashboardScreen(),
-//         floatingActionButton: const FloatingActionButton(
-//           tooltip: 'Add',
-//           onPressed: null,
-//           child: Icon(Icons.add),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
---
+++ lib/res/font.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const lightTextTheme = TextTheme(
+    // displayLarge: GoogleFonts.poppins(textStyle: textTheme.displayLarge),
+    // displayMedium: GoogleFonts.poppins(textStyle: textTheme.displayMedium),
+    // displaySmall: GoogleFonts.poppins(textStyle: textTheme.displaySmall),
+    // headlineLarge: GoogleFonts.poppins(textStyle: textTheme.headlineLarge),
+    // headlineMedium: GoogleFonts.poppins(textStyle: textTheme.headlineMedium),
+    // headlineSmall: GoogleFonts.poppins(textStyle: textTheme.headlineSmall),
+    // titleLarge: GoogleFonts.poppins(textStyle: textTheme.titleLarge),
+    // titleMedium: GoogleFonts.poppins(textStyle: textTheme.titleMedium),
+    // titleSmall: GoogleFonts.poppins(textStyle: textTheme.titleSmall),
+    // bodyLarge: GoogleFonts.poppins(textStyle: textTheme.bodyLarge),
+    // bodyMedium: GoogleFonts.poppins(textStyle: textTheme.bodyMedium),
+    // bodySmall: GoogleFonts.poppins(textStyle: textTheme.bodySmall),
+    // labelLarge: GoogleFonts.poppins(textStyle: textTheme.labelLarge),
+    // labelMedium: GoogleFonts.poppins(textStyle: textTheme.labelMedium),
+    // labelSmall: GoogleFonts.poppins(textStyle: textTheme.labelSmall),
+    );
+
+const darkTextTheme = TextTheme();
