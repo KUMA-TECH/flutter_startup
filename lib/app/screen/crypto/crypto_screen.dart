a371f9a4b26e1d9e47b6ff5d8bb976ec66529fc3
pengfei.huang <huangpengfei@qudian.com>
2023-06-14 10:11:04 +0800

    Fix textfield 动态联动

--- lib/app/screen/crypto/crypto_screen.dart
+++ lib/app/screen/crypto/crypto_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_startup/app/widgets/container/dashboard_header.dart';
+import 'package:flutter_startup/app/widgets/toast.dart';
 import 'package:flutter_startup/res/dimensions.dart';
 import 'package:flutter_startup/utils/crypto.dart';
 
@@ -9,7 +10,12 @@ class CryptoScreen extends StatefulWidget {
   final String title;
   final Crypto crypto;
 
-  CryptoScreen({super.key, required this.title}) : crypto = Crypto();
+  final int maxLines;
+  final int minLines;
+
+  CryptoScreen(
+      {super.key, required this.title, this.minLines = 5, this.maxLines = 30})
+      : crypto = Crypto();
 
   @override
   State<StatefulWidget> createState() => CryptoState();
@@ -46,57 +52,70 @@ class CryptoState extends State<CryptoScreen> {
   }
 
   _buildCrpytoForm() {
-    return Row(children: [
-      Expanded(
-        flex: 1,
-        child: TextField(
-          maxLines: 5,
-          textAlignVertical: TextAlignVertical.top,
-          onChanged: ((value) => _encode(value)),
-          decoration: const InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Text',
-            alignLabelWithHint: true,
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 1,
+            child: TextField(
+              minLines: widget.minLines,
+              maxLines: widget.maxLines,
+              textAlignVertical: TextAlignVertical.top,
+              onChanged: ((value) => _encode(value)),
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Text',
+                alignLabelWithHint: true,
+              ),
+            ),
           ),
-        ),
-      ),
-      Padding(
-        padding: defaultPadding,
-        child: Column(children: [
-          ElevatedButton(
-              onPressed: () => {_encode(_input)}, child: const Text("加密")),
-          const SizedBox(height: defaultPaddingValue),
-          ElevatedButton(
-              onPressed: () => {_decode(_input)}, child: const Text("解密")),
-        ]),
-      ),
-      Expanded(
-        flex: 1,
-        child: TextField(
-          controller: TextEditingController(text: _output),
-          maxLines: 5,
-          // enabled: false,
-          textAlignVertical: TextAlignVertical.top,
-          decoration: InputDecoration(
-            border: const OutlineInputBorder(),
-            labelText: 'Result',
-            alignLabelWithHint: true,
-            suffixIcon: IconButton(
-                onPressed: () {
-                  if (_output?.isEmpty ?? true) return;
-
-                  Clipboard.setData(ClipboardData(text: _output));
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                    content: Text("Result copied",
-                        style: Theme.of(context).textTheme.labelMedium),
-                    backgroundColor: Theme.of(context).colorScheme.surface,
-                  ));
-                },
-                icon: const Icon(Icons.copy)),
+          Padding(
+            padding: defaultPadding,
+            child: Column(children: [
+              ElevatedButton(
+                  onPressed: () => {_encode(_input)}, child: const Text("加密")),
+              const SizedBox(height: defaultPaddingValue),
+              ElevatedButton(
+                  onPressed: () => {_decode(_input)}, child: const Text("解密")),
+            ]),
           ),
-        ),
-      ),
-    ]);
+          Expanded(
+            flex: 1,
+            child: Stack(
+              alignment: Alignment.bottomRight,
+              children: [
+                TextField(
+                  controller: TextEditingController(text: _output),
+                  minLines: widget.minLines,
+                  maxLines: widget.maxLines,
+                  enabled: false,
+                  onChanged: (value) {},
+                  textAlignVertical: TextAlignVertical.top,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Result',
+                    alignLabelWithHint: true,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    right: defaultPaddingValue / 2,
+                    bottom: defaultPaddingValue / 2,
+                  ),
+                  child: IconButton(
+                    onPressed: () {
+                      if (_output?.isEmpty ?? true) return;
+                      Clipboard.setData(ClipboardData(text: _output));
+                      toast(context, "Result copied");
+                    },
+                    icon: const Icon(Icons.copy),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ]);
   }
 
   @override
